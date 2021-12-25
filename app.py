028ff0be3b582cc32079138315e99b7be2cfd7f6
Jonny Nguyen <jonnyn@live.com>
2021-12-25 14:39:09 -0800

    feat: add geometry spatial data type

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from apispec.ext.marshmallow import MarshmallowPlugin
 from apispec_webframeworks.flask import FlaskPlugin
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import func
+from geoalchemy2 import Geometry
 from datetime import datetime
 from marshmallow import Schema, fields
-
-# from sqlalchemy import Column
-# from geoalchemy2 import Geometry
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
@@ -16,14 +15,15 @@ app.config["CORS_HEADERS"] = "Content-Type"
 app.config[
     "SQLALCHEMY_DATABASE_URI"
 ] = "postgresql://postgres:8YY7mwmU905NAJ8oGHJK@database-1.cznvnak7l6zj.us-west-2.rds.amazonaws.com:5432/stations"
+app.config["SQLALCHEMY_ECHO"] = False
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
 
 class Station(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # geometry = Column(Geometry(geometry_type='POINT'), nullable = False)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    geometry = db.Column(Geometry(geometry_type="POINT"))
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
     address = db.Column(db.String(600), nullable=False)
@@ -33,7 +33,15 @@ class Station(db.Model):
     createdAt = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
 
     def __repr__(self):
-        return self.name
+        return "<Station {id} ({lat}, {lon})>".format(
+            id=self.id, lat=self.latitude, lon=self.longitude
+        )
+
+    def get_stations_within_radius(self, radius):
+        """Return all stations within a given radius (in meters)."""
+        return Station.query.filter(
+            func.ST_Distance_Sphere(Station.geometry, self.geometry) < radius
+        ).all()
 
     @classmethod
     def get_all(cls):
@@ -42,6 +50,15 @@ class Station(db.Model):
     @classmethod
     def get_by_id(cls, id):
         return cls.query.get_or_404(id)
+
+    @classmethod
+    def update_geometries(cls):
+        """Using each station's longitude and latitude, add geometry data to db."""
+        stations = Station.query.all()
+        for station in stations:
+            point = "POINT({} {})".format(station.longitude, station.latitude)
+            station.geometry = point
+        db.session.commit()
 
     def save(self):
         db.session.add(self)
@@ -54,7 +71,7 @@ class Station(db.Model):
 
 class StationSchema(Schema):
     id = fields.Integer()
-    # geometry = fields.String()
+    geometry = fields.String()
     latitude = fields.Float()
     longitude = fields.Float()
     address = fields.String()
@@ -149,6 +166,7 @@ def create_a_station():
     new_station = Station(
         latitude=data.get("latitude"),
         longitude=data.get("longitude"),
+        geometry="POINT({} {})".format(data.get("longitude"), data.get("latitude")),
         address=data.get("address"),
         provider=data.get("provider"),
         quantity=data.get("quantity"),
@@ -191,12 +209,15 @@ def update_station():
     id = request.args.get("id")
     updating_station = Station.get_by_id(id)
     data = request.get_json()
-    updating_station.latitude = (data.get("latitude"),)
-    updating_station.longitude = (data.get("longitude"),)
-    updating_station.address = (data.get("address"),)
-    updating_station.provider = (data.get("provider"),)
-    updating_station.quantity = (data.get("quantity"),)
+    updating_station.latitude = data.get("latitude")
+    updating_station.longitude = data.get("longitude")
+    updating_station.address = data.get("address")
+    updating_station.provider = data.get("provider")
+    updating_station.quantity = data.get("quantity")
     updating_station.availability = data.get("availability")
+    updating_station.geometry = "POINT({} {})".format(
+        updating_station.longitude, updating_station.latitude
+    )
     db.session.commit()
     serializer = StationSchema()
     data = serializer.dump(updating_station)
